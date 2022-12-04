31b0a725e603897d55aaae66c66734bd16271822
Gamerize <gundam.issacwai@gmail.com>
2022-12-04 23:06:55 +0000

    Added new script

--- MobileDevGame/Assets/Scripts/TurnSystem.cs
+++ MobileDevGame/Assets/Scripts/TurnSystem.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum BattleState { Start, DiceRoll, Player1Turn, Player2Turn, Player1Win, Player2Win}
+public enum BattleState { StartPhase, DicePhase, Player1Turn, Player2Turn, Player1Win, Player2Win}
 
 public class TurnSystem : MonoBehaviour
 {
-    public BattleState state;
+    public BattleState state; 
 
     // Start is called before the first frame update
     void Start()
     {
-        state = BattleState.Start; 
-        
+        state = BattleState.StartPhase; 
+        SetupMatch();
     }
 
-    void RollDice()
+    void SetupMatch()
     {
-        state = BattleState.DiceRoll;
+        DicePhase();
+    }
 
+    void DicePhase()
+    {
+        state = BattleState.DicePhase;
+        int m_P1DiceRoll = RollDice.m_P1DiceNum;
+        int m_P2DiceRoll = RollDice.m_P2DiceNum;
+        do
+        {
+            if (m_P1DiceRoll > m_P2DiceRoll)
+            {
+                state = BattleState.Player1Turn;
+                Debug.Log("Player 1 Turn");
+            }
+            else if (m_P1DiceRoll < m_P2DiceRoll)
+            {
+                state = BattleState.Player2Turn;
+                Debug.Log("Player 2 Turn");
+            }
+        } while (m_P1DiceRoll == m_P2DiceRoll);
     }
 }
